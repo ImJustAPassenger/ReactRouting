38964fcc67a565637f3bdfca2731dad5d3e24e67
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-22 10:44:30 +0200

    now action are more dinamic, added the patch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,16 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Event, { loader as eventLoader } from "./pages/Event";
-import EventDetail, { loader as eventDetailLoader, action as deleteEventAction } from "./pages/EventDetail";
-import NewEvent,{action as newEventAction} from "./pages/NewEvent";
+import EventDetail, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from "./pages/EventDetail";
+import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import Root from "./pages/Root";
 import EventRoot from "./pages/EventsRoot";
 import ErrorPage from "./pages/ErrorPage";
-
+import { action as manipulateEventAction } from "./components/EventForm";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -48,19 +29,23 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            id:'event-detail',
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
                 index: true,
                 element: <EventDetail />,
-                action:deleteEventAction
+                action: deleteEventAction,
               },
-              { path: "edit", element: <EditEvent /> },
+              {
+                path: "edit",
+                element: <EditEvent />,
+                action: manipulateEventAction,
+              },
             ],
           },
 
-          { path: "new", element: <NewEvent />, action:newEventAction },
+          { path: "new", element: <NewEvent />, action: manipulateEventAction },
         ],
       },
     ],
